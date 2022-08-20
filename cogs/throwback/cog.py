72c0cec29b437b3e6079a9debc2862432ddad614
Taylor Amy <taylorsamy@users.noreply.github.com>
2022-08-20 16:07:40 -0600

    cooldown

--- cogs/throwback/cog.py
+++ cogs/throwback/cog.py
@@ -1,13 +1,16 @@
 from nextcord.ext import commands
 import nextcord
+import asyncio, random
 
 class Throwback(commands.Cog, name="throwback"):
     """Receives ping commands"""
 
     COG_EMOJI = "🏓"
+    
 
     def __init__(self, bot: commands.Bot):
         self._bot = bot
+        self.__cooldown = []
 
     @commands.command(hidden=True)
     async def throwback(self, ctx: commands.Context):
@@ -18,21 +21,48 @@ class Throwback(commands.Cog, name="throwback"):
     async def on_message(self, message: nextcord.Message):
 
         if message.author.id != self._bot.user.id:
-            if "ship" in message.content.lower() and "friendship" not in message.content.lower()\
-            and "partnership" not in message.content.lower() and "companionship" not in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean token?") 
+            items = []
+            if " ship" in message.content.lower() or message.content.startswith("ship"):
+                if "ships" in message.content.lower():
+                    items.append("tokens")
+                else:
+                    items.append("token")
             if "colony" in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean base?")
+                if "colonies" in message.content.lower():
+                    items.append("bases")
+                else:
+                    items.append("base")
             if "encounter" in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean challenge?")
+                if "encounters" in message.content.lower():
+                    items.append("challenges")
+                else:
+                    items.append("challenge")
             if "artifact" in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean edict?")
+                if "artifacts" in message.content.lower():
+                    items.append("edicts")
+                else:
+                    items.append("edict")
             if "hyperspace gate" in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean the cone?")
+                items.append("cone")
             if "compensation" in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean consolation?")
+                items.append("consolation")
             if "negotiate" in message.content.lower():
-                await message.channel.send(message.author.mention +  " Don't you mean compromise?")    
+                items.append("compromise")
+
+            if items and self.__cooldown.count(message.author.id) < 1:
+                # exit 25% of the time
+                if random.randint(1, 4) == 1:
+                    return
+                
+                response = "Don't you mean " + ", ".join(items) + "?"
+                await message.channel.send(response)
+                self.__cooldown.append(message.author.id)
+                await asyncio.sleep(300)
+                self.__cooldown.remove(message.author.id)
+                # print (response)
+
+            
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Throwback(bot))
